c5340ae4af6abfb35bb752f44fa082e803000d62
Daviizy.05 <Daviizy05@gmail.com>
2024-07-15 10:16:30 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
+// App.js
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './Home';
 import WalletConnectPage from './WalletConnectPage';
 import Header from './Header';
 import MainPage from './MainPage';
 
 function ScrollToTop() {
-  useEffect(() => {
-    window.scrollTo(0, 0); // Scrolls to the top when the component mounts
-  }, []);
+  const { pathname } = useLocation();
 
-  return null; // Renders nothing (purely for side effect)
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-gray-100">
         <nav className="bg-white shadow-md p-4">
           <Header />
         </nav>
-
         <Routes>
-          {/* Wrap your Routes in ScrollToTop component */}
-          <ScrollToTop />
           <Route path="/" element={<MainPage />} />
           <Route path="/connect" element={<Home />} />
           <Route path="/connect-wallet" element={<WalletConnectPage />} />
